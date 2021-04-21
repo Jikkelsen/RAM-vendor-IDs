f3fc59bd9a511d03af956eb2a16b64c5a109aab7
Jikkelsen <Mikkelsen.v.jonas@gmail.com>
2021-04-21 16:07:27 +0200

    Added lists

---
+++ Vendorlists.ps1
@@ -0,0 +1,10 @@
+$Vendors = @{
+    "0x014F" = "Transcend Information"
+    "0x2C00" = "Micron Technology"
+    "0x802C" = "Micron Technology"
+    "0x80AD" = "Hynix Semiconductor"
+    "0x80CE" = "Samsung Electronics"
+    "0xAD00" = "Hynix Semiconductor"
+    "0xCE00" = "Samsung Electronics"
+    "0x02FE" = "Elpida"
+}
